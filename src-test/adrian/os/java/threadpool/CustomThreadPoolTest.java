9595ae2086e3b8d261ab920b598d915708174c9f
Adrian-26-Isotope <45631612+Adrian-26-Isotope@users.noreply.github.com>
2026-03-17 14:48:56 +0100

    Update CustomThreadPoolTest.java

--- src-test/adrian/os/java/threadpool/CustomThreadPoolTest.java
+++ src-test/adrian/os/java/threadpool/CustomThreadPoolTest.java
@@ -1,6 +1,6 @@
 package adrian.os.java.threadpool;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
